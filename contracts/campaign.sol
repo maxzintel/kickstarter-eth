35b5d714b14f82610862ba9160ee6042fffa6f7d
s1dequest <maxzintel@gmail.com>
2022-07-17 22:14:41 -0400

    Code wireframes

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -3,13 +3,36 @@ pragma solidity ^0.4.17;
 contract Campaign {
   address public manager;
   address[] public approvers;
+  uint public minContribution;
 
-  modifier restricted() {
+  modifier restrictManager() {
     require(msg.sender == manager);
+    _;
+  }
+
+  modifier restrictApprover() {
+    //require msg.sender to be in the array of approvers.
     _;
   }
 
    constructor() public {
     manager = msg.sender;
+  }
+
+  function contribute() public payable {
+    require(msg.value > 0.01 ether);
+    approvers.push(msg.sender); // This needs logic to discern if someone has already been added to the list.
+  }
+
+  function createRequest() public restrictManager {
+
+  }
+
+  function approveRequest() public restrictApprover {
+
+  }
+
+  function finalizeRequest() public restrictManager {
+    
   }
 }
