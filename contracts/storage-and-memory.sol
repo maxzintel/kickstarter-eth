60746506a214adca1cec672a3d8ea3547b35b040
s1dequest <maxzintel@gmail.com>
2022-07-23 12:27:16 -0400

    Add changearray function.

--- contracts/storage-and-memory.sol
+++ contracts/storage-and-memory.sol
@@ -15,9 +15,18 @@ contract StorageAndMemory {
     // [1, 32]
 
     // now myArray points towards a copy of int[] stored in memory.
-    int[] memory myArray = numbers;
-    myArray[0] = 1;
+    // int[] memory myArray = numbers;
+    // myArray[0] = 1;
     // now, getting the output of NUMBERS returns the original array [20,32]
     // getting the output of MYARRAY outputs [1,32]
+    //________________________________//
+
+    changeArray(numbers);
+  }
+
+  // by default, similar to memory, myArray is a copy of the numbers array given it takes the numbers array as an arg from above.
+  // basically, int[] memory is the default.
+  function changeArray(int[] storage myArray) private {
+    myArray[0] = 1;
   }
 }
