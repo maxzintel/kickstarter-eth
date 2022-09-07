f4d8377fa52aec6d5688c749424b808c3a5f88ae
maxzintel <maxzintel@gmail.com>
2022-09-07 10:50:10 -0400

    Fix compile script.

--- compile.js
+++ compile.js
@@ -1,41 +1,39 @@
-const path = require('path'); // cross-platform compatibility between windows and unix.
-const fs = require('fs-extra');
-const solc = require('solc');
-
-// Find and remove the tmp build directory and its contents.
-// See readme for details on why we do this.
+const path = require("path");
+const fs = require("fs-extra");
+const solc = require("solc");
+ 
 const buildPath = path.resolve(__dirname, 'build');
-fs.removeSync(buildPath);
-
-// Get and compile the contracts.
-const campaignPath = path.resolve(__dirname, 'contracts', 'campaign.sol');
-const source = fs.readFileSync(campaignPath, 'utf8');
-var input = {
-  language: 'Solidity',
-  sources: 
-  {
-    'campaign.sol': {
-      content: source
-    }
+fs.removeSync(buildPath); // removes the folder
+fs.ensureDirSync(buildPath); // checks if directory exists, otherwise it creates it.
+ 
+ 
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+ 
+ 
+const input = {
+  language: "Solidity",
+  sources: {
+    "Campaign.sol": {
+      content: source,
+    },
   },
   settings: {
     outputSelection: {
-      '*': {
-        '*': ['*']
-      }
-    }
-  }
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
 };
-
-var output = JSON.parse(solc.compile(JSON.stringify(input)));
-// const output = solc.compile(source, 1).contracts;
-
-// Checks to see if the dir exists and if not creates it for us.
-fs.ensureDirSync(buildPath);
-
-// output the contracts to our tmp directory.
+ 
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'];
 console.log(output);
-for (let contract in output) {
+ 
+fs.ensureDirSync(buildPath); // checks if directory exists, otherwise it creates it.
+ 
+for(let contract in output){
+  console.log(contract);
   fs.outputJsonSync(
     path.resolve(buildPath, contract + '.json'),
     output[contract]
--- test/contracts.test.js
+++ test/contracts.test.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
-const { exec } = require('child_process');
 const ganache = require('ganache-cli');
-const Web3 = require('web3'); // Capital W means we are the constructor here.
-const web3 = new Web3(ganache.provider()); // instantiating the constructor above. attempts to connect to local test network. Different providers will be set for 'production' networks.
+const Web3 = require('web3'); // Capital W means we are using the constructor here.
+const web3 = new Web3(ganache.provider()); // instantiating the constructor above. Attempts to connect to local test network. Different providers will be set for 'production' networks.
 const { interface, bytecode } = require('../compile');
+
+const compiledFactory = require('../contracts/build')
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
