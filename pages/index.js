4761d96d50a82f7121f560f759beadef0cfc623d
maxzintel <maxzintel@gmail.com>
2022-09-19 14:18:43 -0400

    Refactor index.js

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,22 @@ import React, { Component } from "react";
 // import factory instance
 import factory from '../web3/factory';
 
-// export async function getStaticProps(context) {
-
-// }
-
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  // componentDidMount is appropriate for data fetching in traditional react apps
+  // BUT since we are using next & since we want to fetch data on the next server...
+  // we ACTUALLY want to use getInitialProps
+  // getInitialProps is the first and only thing the next server will do, it will not attempt to render anything.
+
+  // static = class function => does not require an instance to call this method.
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    // console.log(campaigns);
+    // below object is provided to the component as props
+    return { campaigns };
   }
 
   render() {
-    return <h1>Campaigns Index!</h1>
+    return <h1>{this.props.campaigns[0]}</h1>
   }
 }
 
