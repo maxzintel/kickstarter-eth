5cb4090ec154dfd9a045a6e28e53069cc5b6c1c2
maxzintel <maxzintel@gmail.com>
2022-09-25 18:14:40 -0400

    campaigns/new frontend bits before we set it up with event handlers.

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 // import 'semantic-ui-css/semantic.min.css';
 import Layout from '../../components/layout';
+import { Form, Button, Input } from 'semantic-ui-react';
 
 class CampaignNew extends Component {
   render() {
     return (
       <Layout>
-        <div>
-        </div>
+        <h3>Create a Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label='wei'
+              labelPosition='right'
+              placeholder='Ex: 1000'
+            />
+          </Form.Field>
+
+          <Button primary loading>Create</Button>
+        </Form>
       </Layout>
     );
   }
