aa2ecc038fd42eaeb090dbc8051ae808389c08cf
maxzintel <maxzintel@gmail.com>
2022-09-28 12:04:35 -0400

    Begin implementing dynamic routing.

---
+++ pages/campaigns/show.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Layout from '../../components/layout';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
+import factory from '../../web3/factory';
+import web3 from '../../web3/web3';
+import { Link, Router } from '../../routes';
+
+class CampaignShow extends Component {
+  render() {
+    return (
+      <Layout>
+        <h3>howdy!</h3>
+      </Layout>
+    )
+  }
+}
+
+export default CampaignShow;
--- routes.js
+++ routes.js
@@ -2,6 +2,12 @@
 // require statement here returns a function that is invoked immediately after we require it into this file.
 const routes = require('next-routes')();
 
+// Routes higher up take precedent over routes lower in hierarchy.
+routes
+  .add('/campaigns/new', '/campaigns/new')
+// :address means basically a wildcard variable we will name address.
+  .add('/campaigns/:address', '/campaigns/show');
+
 // exports helpers that allow us to automatically navigate users around the app
 module.exports = routes;
 
