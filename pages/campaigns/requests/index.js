105231ffe7b0eed9f15b8b6d87f77ec8c179e974
maxzintel <maxzintel@gmail.com>
2022-10-06 10:19:12 -0400

    Begin adding logic to return requests.

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,20 +1,40 @@
 import React, {Component} from "react";
 import Layout from "../../../components/layout";
 import { Link } from "../../../routes";
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import Campaign from '../../../web3/campaign';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
+    const campaign = Campaign(address);
+    // the plus here converts this return value to an int.
+    // otherwise, requests only returns 1 element because the number is simply a string rather than an actual int it can measure length with.
+    const requestCount = +await campaign.methods.getRequestsCount().call();
 
-    return { address };
+    // Create array of returned structs(console.log it)
+    // What we are trying to do here is iterate from 0 to the value of request count.
+    const requests = await Promise.all(
+      Array(requestCount).fill().map((element, index) => {
+        return campaign.methods.requests(index).call()
+      })
+    );
+
+    return { address, requests, requestCount };
   }
 
+  // We need to setup an array of structs
+  // Request is a struct in our contract code.
+
+  // renderRequests() {
+  //   const items = this.props.campaign.
+  // }
+
   render() {
+    // es2015 destructuring
+    const {Header, Row, HeaderCell, Body} = Table;
     return (
       <Layout>
-        <h3>Requests</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
             <Button primary floated="right" style={{ marginBottom: 10 }}>
@@ -22,6 +42,21 @@ class RequestIndex extends Component {
             </Button>
           </a>
         </Link>
+        <Table striped>
+          <Header>
+            <Row>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Completed</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+            </Row>
+          </Header>
+
+          <Body>
+
+          </Body>
+        </Table>
       </Layout>
     );
   }
